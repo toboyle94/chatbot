1718f86689de65656a561895d40deeb31799b227
Thomas O'Boyle <tomjoboyle@gmail.com>
2020-06-08 22:25:10 -0400

    add views and urls to display question and show diffs against text over time

--- django_project/chatbot/views.py
+++ django_project/chatbot/views.py
@@ -1,3 +1,28 @@
-from django.shortcuts import render
+from difflib import ndiff
 
-# Create your views here.
+from django.views.generic import ListView, DetailView
+from chatbot.models import Question, QuestionText
+
+
+class QuestionList(ListView):
+    model = Question
+    template_name = 'question_list.html'
+    context_object_name = 'questions'
+
+
+class QuestionDetail(DetailView):
+    model = Question
+    template_name = 'question.html'
+    slug_url_kwarg = 'id'
+    slug_field = 'id'
+
+    def get_context_data(self, **kwargs):
+        context_data = super().get_context_data()
+
+        previous_text_version_diffs = []
+        for question_text in self.object.text_variations.order_by('-created')[1:]:
+            text_diff = ndiff(question_text.text.split(), self.object.current_text.split())
+            previous_text_version_diffs.append(text_diff)
+
+        context_data['previous_text_version_diffs'] = previous_text_version_diffs
+        return context_data
--- django_project/django_project/urls.py
+++ django_project/django_project/urls.py
@@ -16,6 +16,11 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+from chatbot.views import QuestionList, QuestionDetail
+
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('question/<int:id>/', QuestionDetail.as_view(), name='question'),
+    path('question/', QuestionList.as_view()),
 ]
